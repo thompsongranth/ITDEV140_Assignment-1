1d12aaa958a01142da0977000fca8de86748e57f
Grant Thompson <thompsongranth@yahoo.com>
2017-01-30 19:02:14 -0600

    Assignment#1

---
+++ src/thompson_myfirstjavaapp/Thompson_MyFirstJavaApp.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thompson_myfirstjavaapp;
+
+/**
+ *
+ * @author Grant Thompson
+ * ITDEV 140 THUR EVE
+ * ASSIGNMENT 1
+ */
+public class Thompson_MyFirstJavaApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Controller controller_1 = new Controller();
+    }
+    
+}
